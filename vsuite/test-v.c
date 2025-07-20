143a5bfefa0a7499d8ce1e354dcd896f1ba24efe
phdye <phdye@acm.org>
2025-07-20 05:47:14 -0400

    Add test suites for v and zv headers

---
+++ vsuite/test-v.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <string.h>
+#include "v.h"
+
+static int failures = 0;
+static int verbose = 0;
+
+#define CHECK(name, expr) do { \
+    if (!(expr)) { \
+        printf("\nFAIL: %s\n", name); \
+        failures++; \
+    } else if (verbose) { \
+        printf("PASS: %s\n", name); \
+    } else { \
+        fputc('.', stdout); fflush(stdout); \
+    } \
+} while (0)
+
+static void test_init_clear(void) {
+    DECL_VARCHAR(v, 5);
+    memset(v.arr, 'x', sizeof(v.arr));
+    v.len = 3;
+    v_init(v);
+    CHECK("v_init", v.len == 0);
+
+    v.len = 4;
+    v_clear(v);
+    CHECK("v_clear", v.len == 0);
+}
+
+static void test_valid(void) {
+    DECL_VARCHAR(v, 5);
+    v.len = 5;
+    CHECK("v_valid ok", v_valid(v));
+    v.len = 6;
+    CHECK("v_valid overflow", !v_valid(v));
+}
+
+static void test_copy(void) {
+    DECL_VARCHAR(src, 6); DECL_VARCHAR(dst, 6); DECL_VARCHAR(small, 2);
+    strcpy(src.arr, "abc"); src.len = 3;
+    int n = v_copy(dst, src);
+    CHECK("v_copy len", n == 3 && dst.len == 3 && memcmp(dst.arr, "abc", 3) == 0);
+
+    n = v_copy(small, src);
+    CHECK("v_copy fail", n == 0 && small.len == 0);
+}
+
+static void test_trim(void) {
+    DECL_VARCHAR(v1, 10); DECL_VARCHAR(v2, 10); DECL_VARCHAR(v3, 10);
+    strcpy(v1.arr, "  hi"); v1.len = 4; v_ltrim(v1);
+    CHECK("v_ltrim", v1.len == 2 && memcmp(v1.arr, "hi", 2) == 0);
+
+    strcpy(v2.arr, "hi  "); v2.len = 4; v_rtrim(v2);
+    CHECK("v_rtrim", v2.len == 2 && memcmp(v2.arr, "hi", 2) == 0);
+
+    strcpy(v3.arr, "  hi  "); v3.len = 6; v_trim(v3);
+    CHECK("v_trim", v3.len == 2 && memcmp(v3.arr, "hi", 2) == 0);
+}
+
+static void test_case(void) {
+    DECL_VARCHAR(v, 4);
+    strcpy(v.arr, "aB3"); v.len = 3;
+    v_upper(v);
+    CHECK("v_upper", strcmp(v.arr, "AB3") == 0 && v.len == 3);
+    v_lower(v);
+    CHECK("v_lower", strcmp(v.arr, "ab3") == 0 && v.len == 3);
+}
+
+int main(int argc, char **argv) {
+    for (int i=1;i<argc;i++) if (!strcmp(argv[i], "-v") || !strcmp(argv[i], "--verbose")) verbose = 1;
+    test_init_clear();
+    test_valid();
+    test_copy();
+    test_trim();
+    test_case();
+    if (failures == 0) {
+        printf(verbose ? "\nAll tests passed.\n" : "\n");
+    } else {
+        printf("\n%d test(s) failed.\n", failures);
+    }
+    return failures;
+}
