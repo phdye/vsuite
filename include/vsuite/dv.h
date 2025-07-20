a6b334f185a0bf1f34da4705ecc3e189949e2f8b
philip-h-dye <philip.h.dye@gmail.com>
2025-07-20 18:29:10 -0400

    {dv,vd}.h : swap contents and correct prefixes.  Prefix meaning <dst><src>

--- include/vsuite/dv.h
+++ include/vsuite/dv.h
@@ -4,16 +4,28 @@
 #include <string.h>
 #include <stddef.h>
 
-/* Copy from Dynamic C String to Fixed VARCHAR */
-#define vd_copy(vdst, dsrc)                                                   \
+/* Copy from Fixed VARCHAR to Dynamic C String (preallocated dest) */
+#define dv_copy(dstr, dcap, vsrc)                                             \
     do {                                                                      \
-        size_t __n = strlen(dsrc);                                            \
-        if (__n < V_SIZE(vdst)) {                                             \
-            memcpy(V_BUF(vdst), dsrc, __n);                                   \
-            (vdst).len = __n;                                                 \
-        } else {                                                              \
-            (vdst).len = 0;                                                   \
+        if ((vsrc).len < (dcap)) {                                            \
+            memcpy(dstr, V_BUF(vsrc), (vsrc).len);                            \
+            dstr[(vsrc).len] = '\0';                                          \
+        } else if ((dcap) > 0) {                                              \
+            dstr[0] = '\0';                                                   \
         }                                                                     \
     } while (0)
+
+
+#define dv_dup(v) dv_dup_fcn(V_BUF(v), (v).len)
+
+/* Duplicate Fixed VARCHAR into newly allocated Dynamic C String */
+static inline char *dv_dup_fcn(const char *src_buf, unsigned short src_len)
+{
+    char *d = malloc(src_len + 1);
+    if (!d) return NULL;
+    memcpy(d, src_buf, src_len);
+    d[src_len] = '\0';
+    return d;
+}    
 
 #endif /* VSUITE_DV_H */
--- include/vsuite/vd.h
+++ include/vsuite/vd.h
@@ -7,29 +7,16 @@
 
 #include <vsuite/v.h>
 
-/* Copy from Fixed VARCHAR to Dynamic C String (preallocated dest) */
-#define dv_copy(dstr, dcap, vsrc)                                             \
+/* Copy from Dynamic C String to Fixed VARCHAR */
+#define vd_copy(vdst, dsrc)                                                   \
     do {                                                                      \
-        if ((vsrc).len < (dcap)) {                                            \
-            memcpy(dstr, V_BUF(vsrc), (vsrc).len);                            \
-            dstr[(vsrc).len] = '\0';                                          \
-        } else if ((dcap) > 0) {                                              \
-            dstr[0] = '\0';                                                   \
+        size_t __n = strlen(dsrc);                                            \
+        if (__n < V_SIZE(vdst)) {                                             \
+            memcpy(V_BUF(vdst), dsrc, __n);                                   \
+            (vdst).len = __n;                                                 \
+        } else {                                                              \
+            (vdst).len = 0;                                                   \
         }                                                                     \
     } while (0)
-
-
-#define vd_dup(v) vd_dup_fcn(V_BUF(v), (v).len)
-
-/* Duplicate Fixed VARCHAR into newly allocated Dynamic C String */
-static inline char *vd_dup_fcn(const char *src_buf, unsigned short src_len)
-{
-    char *d = malloc(src_len + 1);
-    if (!d) return NULL;
-    memcpy(d, src_buf, src_len);
-    d[src_len] = '\0';
-    return d;
-}
-
 
 #endif /* VSUITE_VD_H */
