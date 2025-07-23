930d54f613413b1746a01c8a0206b400b7fad2d9
phdye <phdye@acm.org>
2025-07-23 09:56:25 -0400

    Port better-varchar to Python 3.2

--- tests/test_better_varchar.py
+++ tests/test_better_varchar.py
@@ -1,12 +1,13 @@
 import unittest
-import importlib.util
 import os
+import types
 
-SPEC = importlib.util.spec_from_file_location(
-    "better_varchar", os.path.join(os.path.dirname(__file__), "..", "better-varchar.py")
-)
-better_varchar = importlib.util.module_from_spec(SPEC)
-SPEC.loader.exec_module(better_varchar)
+module_path = os.path.join(os.path.dirname(__file__), "..", "better-varchar.py")
+better_varchar = types.ModuleType("better_varchar")
+better_varchar.__file__ = module_path
+with open(module_path, "r") as fh:
+    code = compile(fh.read(), module_path, "exec")
+exec(code, better_varchar.__dict__)
 
 class TestBetterVarchar(unittest.TestCase):
     """Tests for the better-varchar source transformation helpers."""
@@ -27,6 +28,19 @@ class TestBetterVarchar(unittest.TestCase):
         src = 'strcpy(foo.arr, "hi");'
         out = better_varchar.transform(src)
         self.assertIn('vp_copy(foo, "hi");', out)
+
+    def test_only_filter(self):
+        src = (
+            "strcpy(foo.arr, bar.arr);\nfoo.arr[bar.len] = '\0';\n"
+            "foo.arr[foo.len] = '\0';"
+        )
+        out = better_varchar.transform(src, only=['setlenz'])
+        self.assertIn('VARCHAR_SETLENZ(foo);', out)
+        self.assertNotIn('v_copy(foo, bar);', out)
+
+    def test_parse_only(self):
+        args = better_varchar.parse_args(['--only:vp_copy', 'in.pc', 'out.pc'])
+        self.assertEqual(args.only, ['vp_copy'])
 
 if __name__ == '__main__':
     unittest.main()
