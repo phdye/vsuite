a977f3ccbcde6b013e8a9b07f288763fb27a84d2
Philip Dye <phdye@tva.gov>
2025-07-26 20:59:14 +0100

    pstr.h : updated vp_copy(), zvp_vopy(), pv_copy() with V_WARN

--- include/vsuite/pstr.h
+++ include/vsuite/pstr.h
@@ -16,25 +16,41 @@
  * buffer.  Otherwise ``vdst.len`` is cleared to zero so callers can detect the
  * overflow.
  */
-#define vp_copy(vdst, dsrc)                                                   \
-    do {                                                                      \
-        size_t __n = strlen(dsrc);                                            \
-        if (__n < V_SIZE(vdst)) {                                             \
-            memcpy(V_BUF(vdst), dsrc, __n);                                   \
-            (vdst).len = __n;                                                 \
-        } else {                                                              \
-            (vdst).len = 0;                                                   \
-        }                                                                     \
-    } while (0)
+#define vp_copy(vdst, dsrc)                                          \
+    ({                                                               \
+        varchar_overflow = 0;                                        \
+        size_t siz = V_SIZE(vdst);                                   \
+        size_t __n = strlen(dsrc);                                   \
+        if (__n > siz) {                                             \
+            varchar_overflow = __n - siz;                            \
+            V_WARN("Line %d : vp_copy(%s, %s) : overflow : bytes required %zu > %zu capacity", \
+                __LINE__, #vdst, #dsrc, __n, siz);                   \
+            __n = siz;                                               \
+        }                                                            \
+        memmove(V_BUF(vdst), dsrc, __n);                             \
+        __n;                                                         \
+    })
 
 /*
  * zvp_copy() - Copy a C string pointer and always NUL terminate the result.
  */
-#define zvp_copy(vdst, dsrc)                                                  \
-    do {                                                                      \
-        vp_copy(vdst, dsrc);                                                  \
-        zv_zero_terminate(vdst);                                              \
-    } while (0)
+#define zvp_copy(vdst, dsrc)                                         \
+    ({                                                               \
+        varchar_overflow = 0;                                        \
+        size_t __cap = ZV_CAPACITY(vdst);                            \
+        size_t __n = strlen(dsrc);                                   \
+        if (__n > __cap) {                                           \
+            varchar_overflow = __n - __cap;                          \
+            V_WARN("Line %d : zvp_copy(%s, %s) : overflow : bytes required %zu > %zu capacity", \
+                  __LINE__, #vdst, #dsrc, __n, V_SIZE(vdst));        \
+            __n = __cap;                                             \
+        }                                                            \
+        memmove(V_BUF(vdst), dsrc, __n);                             \
+        (vdst).len = __n;                                            \
+        if (V_SIZE(vdst) > 0)                                        \
+            V_BUF(vdst)[__n] = '\0';                                 \
+        __n;                                                         \
+    })
 
 /*
  * pv_copy() - Copy a VARCHAR into a preallocated C string buffer.
@@ -43,15 +59,21 @@
  * and a terminator appended.  Otherwise the destination is cleared to an empty
  * string (if ``dcap`` is non-zero).
  */
-#define pv_copy(dstr, dcap, vsrc)                                             \
-    do {                                                                      \
-        if ((vsrc).len < (dcap)) {                                            \
-            memcpy(dstr, V_BUF(vsrc), (vsrc).len);                            \
-            dstr[(vsrc).len] = '\0';                                          \
-        } else if ((dcap) > 0) {                                              \
-            dstr[0] = '\0';                                                   \
-        }                                                                     \
-    } while (0)
+#define pv_copy(dstr, dcap, vsrc)                                    \
+    ({                                                               \
+        varchar_overflow = 0;                                        \
+        size_t __cap = (dcap);                                       \
+        size_t __n = (vsrc).len;                                     \
+        if (__n >= __cap) {                                          \
+            varchar_overflow = __n - __cap + 1;                      \
+            V_WARN("Line %d : pv_copy(%s, %s) : overflow : bytes required %zu > %zu capacity", \
+                  __LINE__, #dstr, #vsrc, __n, __cap);               \
+            __n = __cap - 1;                                         \
+        }                                                            \
+        memmove(dstr, V_BUF(vsrc), __n);                             \
+        dstr[__n] = '\0';                                            \
+        __n + 1;                                                     \
+    })
 
 /* Convenience wrapper to duplicate a VARCHAR into a newly allocated C string */
 #define dv_dup(v) dv_dup_fcn(V_BUF(v), (v).len)
@@ -73,5 +95,14 @@ static inline char *dv_dup_fcn(const char *src_buf, unsigned short src_len)
     d[src_len] = '\0';            /* append NUL terminator */
     return d;
 }
+
+/*
+ * zvp_copy() - Copy a C string pointer and always NUL terminate the result.
+ */
+#define zvp_strcmp(vdst, dsrc)                                                \
+    ({                                                                      \
+        zv_valid(vdst);                                                       \
+        strncmpzv_zero_terminate(vdst);                                              \
+    })
 
 #endif /* VSUITE_PSTR_H */
