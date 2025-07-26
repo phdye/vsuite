0c57ca25d3d97dd576399b49ee480d6b803581a0
philip-h-dye <philip.h.dye@gmail.com>
2025-07-26 06:24:08 -0400

    vsuite.h : add string.h

--- include/vsuite.h
+++ include/vsuite.h
@@ -1,12 +1,14 @@
 #ifndef VSUITE_H
 #define VSUITE_H
 
-#include <vsuite/varchar.h>
+#include <vsuite/varchar.h>     // VARCHAR support without z-byte termination
 
-#include <vsuite/zvarchar.h>
+#include <vsuite/zvarchar.h>    // VARCHAR support with z-byte termination
 
-#include <vsuite/fixed.h>
+#include <vsuite/fixed.h>       // VARCHAR <-> fixed allocation C-string conversion
 
-#include <vsuite/pstr.h>
+#include <vsuite/pstr.h>        // VARCHAR <-> char * conversion
+
+#include <vsuite/string.h>      // Fixed allocation C-string manipulation macros
 
 #endif /* VSUITE_H */
